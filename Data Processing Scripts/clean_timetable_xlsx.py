941334494a44e875a64cdaba16cd98675f6c59da
Mike <mikemorrisroe@gmail.com>
2016-06-23 20:10:46 +0100

    filled in functions untested needs work on separating out the sheets save to csv not functioning yet

--- Data Processing Scripts/clean_timetable_xlsx.py
+++ Data Processing Scripts/clean_timetable_xlsx.py
@@ -16,25 +16,87 @@ def csv_from_excel():
 
     your_csv_file.close()
 
-def remove_double_classes():
+def remove_double_classes(sheet):
 
-    find_mergecells()
+    find_mergecells(sheet)
 
-    unmerge_double_classes()
+    unmerge_double_classes(sheet, mergecells)
+
+    return sheet
 
 
-def clean_timetable():
+def clean_timetable(timetable):
 
-    wb = openpyxl.load_workbook('B0.02 B0.03 B0.04 Timetable.xlsx')
+    wb = openpyxl.load_workbook(timetable)
 
     sheet_separator(wb)
 
-    remove_double_classes()
+    for room in rooms:
+        remove_double_classes(room)
 
 
+def sheet_separator(workbook):
+    """Separates a workbook out into its component sheets, returning a list of the sheets
+
+    Has options to omit specific sheetnames"""
+
+    rooms = []
+    excluded_names = ['All'] # add names to be skipped
+
+    for i in wb.get_sheet_names():
+        if i in excluded_names:
+            continue
+
+        rooms.append(i)
+
+    return rooms
+
+def find_mergecells(sheet):
+    """Takes a sheet and finds all mergecells within it, returning a list of the top left cells"""
+
+    mergecells = []
+
+    for i in range(len(sheet.merged_cell_ranges)):
+
+        range_of_merge = sorted(sheet.merged_cell_ranges)[i]
+
+        # only top left cell in merged cell contains a value, find this by splitting on ':' for 'A1:A2' = 'A1'
+        top_left_cell_value = range_of_merge.split(":")[0]
+
+        # several cells contain nothing but are merged due to format, disregard these.
+        if sheet[top_left_cell_value].value == None:
+            continue
+
+        # create list of all the value-containing cells which begin merge cells
+        mergecells.append(top_left_cell_value)
+
+        return mergecells
 
 
+def unmerge_double_classes(sheet, mergecells):
 
+    """Finds the top-left cell of a mergecell, unmerges it then copies that value into the cell beneath it
+
+    Outputs the modified sheet"""
+
+    # use row offset to remove the top two rows as these contain no classes
+    for row in sheet.iter_rows(row_offset=2):
+        for cell in row:
+            if cell.coordinate in mergecells:
+
+                # assignment just to clean up function
+                cell = cell.coordinate
+
+                # create the cell range so 'A1' becomes 'A1:A2'
+                cell_range = str(cell + ':' + cell[:1] ) + str( int(cell[1:]) + 1)
+
+                sheet.unmerge_cells(cell_range)
+
+                # copy value of first cell into the cell below it
+                bottom_cell = cell_range.split(":")[1]
+                sheet[bottom_cell].value = sheet[cell].value
+
+    return sheet
 
 # open the xlsx file to be operated on
 wb = openpyxl.load_workbook('B0.02 B0.03 B0.04 Timetable.xlsx')
