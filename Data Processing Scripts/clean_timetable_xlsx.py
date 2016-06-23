4aac57d1cf1990a18e97120c446dd4285b8d4656
Mike <mikemorrisroe@gmail.com>
2016-06-23 19:22:36 +0100

    Added structure for functions adapting script to modular format adding docstrings and formatting

--- Data Processing Scripts/clean_timetable_xlsx.py
+++ Data Processing Scripts/clean_timetable_xlsx.py
@@ -1,5 +1,39 @@
 
 import openpyxl
+import xlrd
+import csv
+
+def csv_from_excel():
+    """from so, check"""
+
+    wb = xlrd.open_workbook('your_workbook.xls')
+    sh = wb.sheet_by_name('Sheet1')
+    your_csv_file = open('your_csv_file.csv', 'wb')
+    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    for rownum in range(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
+
+    your_csv_file.close()
+
+def remove_double_classes():
+
+    find_mergecells()
+
+    unmerge_double_classes()
+
+
+def clean_timetable():
+
+    wb = openpyxl.load_workbook('B0.02 B0.03 B0.04 Timetable.xlsx')
+
+    sheet_separator(wb)
+
+    remove_double_classes()
+
+
+
+
 
 
 # open the xlsx file to be operated on
@@ -43,27 +77,31 @@ for i in range(len(sheet.merged_cell_ranges)):
     # only top left cell in merged cell contains a value, find this by splitting on ':' for 'A1:A2' = 'A1'
     top_left_cell_value = range_of_merge.split(":")[0]
 
-
+    # several cells contain nothing but are merged due to format, disregard these.
     if sheet[top_left_cell_value].value == None:
         continue
 
+    # create list of all the value-containing cells which begin merge cells
     mergecells.append(top_left_cell_value)
 
 
 
 # only keep mergecells from the 'module' rows
 
+# use row offset to remove the top two rows as these contain no classes
 for row in sheet.iter_rows(row_offset=2):
     for cell in row:
-        if cell.coordinate == 'N13': # local constraint, need to be checked for future datasets. Just to speed results.
-            break
         if cell.coordinate in mergecells:
 
+            # assignment just to clean up function
             cell = cell.coordinate
+
+            # create the cell range so 'A1' becomes 'A1:A2'
             cell_range = str(cell + ':' + cell[:1] ) + str( int(cell[1:]) + 1)
 
             sheet.unmerge_cells(cell_range)
 
+            # copy value of first cell into the cell below it
             bottom_cell = cell_range.split(":")[1]
             sheet[bottom_cell].value = sheet[cell].value
 
