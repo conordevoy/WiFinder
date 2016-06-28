555154188866044152aab40fe71d257910655ab3
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-06-28 17:47:26 +0100

    restructure for Flask template

---
+++ app/website/WiFinderApp.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import sqlite3
+
+WiFinderApp = Flask(__name__, static_url_path="/static")
+
+WiFinderApp.debug = True
+
+WiFinderApp.db = "wifinderDB.db"
+
+@WiFinderApp.route("/")
+def WiFinderHTML():
+	'''Render HTML template'''
+	return render_template("index.html")
+
+def connectDB():
+	'''DB connection'''
+	return sqlite3.connect(WiFinderApp.db)
+
+if __name__ == "__main__":
+	WiFinderApp.run()
