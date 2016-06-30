d6fc66df85cd91b81223760eaed3586370888f53
Mike <mikemorrisroe@gmail.com>
2016-06-30 17:22:49 +0100

    Formatted file for clarity and to get PyCharm to stop shouting at me about PEP-8

--- app/website/WiFinderApp.py
+++ app/website/WiFinderApp.py
@@ -8,9 +8,11 @@ WiFinderApp.debug = True
 # WiFinderApp.db = "wifinderDB.db"
 db = "wifinderDB.db"
 
+
 def connectDB():
-	'''Connects to an Sqlite3 database'''
-	return sqlite3.connect(db)
+    '''Connects to an Sqlite3 database'''
+    return sqlite3.connect(db)
+
 
 def get_db():
     '''Checks for a DB connection. If not found, calls connect_to_database to establish connection'''
@@ -19,42 +21,47 @@ def get_db():
         db = g._database = connectDB()
     return db
 
+
 @WiFinderApp.route("/")
 def WiFinderHTML():
-	'''Render HTML template'''
-	return render_template("index.html")
+    '''Render HTML template'''
+    return render_template("index.html")
+
 
 @WiFinderApp.route("/searchfake")
 def search_page():
-	'''Load demo search page - artificial data to test jinja connections'''
+    '''Load demo search page - artificial data to test jinja connections'''
 
-	return render_template("search.html",
-						   title = 'Search',
-						   rooms = ['B002', 'quack', 'B004'],
-						   times = [9, 10, 11, 12 ,13 ,14 ,15 ,16 ,17, 18, 19],
-						   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-						   densities = ['0%', '25%', '50%', '75%', '100%'])
+    return render_template("search.html",
+                           title='Search',
+                           rooms=['B002', 'quack', 'B004'],
+                           times=[9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+                           days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                           densities=['0%', '25%', '50%', '75%', '100%'])
+
 
 @WiFinderApp.route("/searchreal")
 def search_real():
-	'''load search page with real data - test to check db connection'''
+    '''load search page with real data - test to check db connection'''
 
-	cur = get_db().cursor()
-	roomdata = cur.execute("SELECT * FROM B02 WHERE pid < 10;")
+    cur = get_db().cursor()
+    roomdata = cur.execute("SELECT * FROM B02 WHERE pid < 10;")
 
-	return render_template("search.html",
-						   title = 'Search',
-						   rooms = roomdata,
-						   times = [9, 10, 11, 12 ,13 ,14 ,15 ,16 ,17, 18, 19],
-						   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-						   densities = ['0%', '25%', '50%', '75%', '100%'])
+    return render_template("search.html",
+                           title='Search',
+                           rooms=roomdata,
+                           times=[9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+                           days=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+                           densities=['0%', '25%', '50%', '75%', '100%'])
+
 
 @WiFinderApp.route("/layout")
 def layout():
-	'''load base layout - just to display basic template, not intended as standalone page'''
+    '''load base layout - just to display basic template, not intended as standalone page'''
 
-	return render_template("page_layout.html",
-						   title = 'Layout')
+    return render_template("page_layout.html",
+                           title='Layout')
+
 
 if __name__ == "__main__":
-	WiFinderApp.run()
+    WiFinderApp.run()
