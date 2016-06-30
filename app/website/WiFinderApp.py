7144e17d6fd63a708d0487ca1fbefb170723533a
Mike <mikemorrisroe@gmail.com>
2016-06-30 16:31:14 +0100

    Reworked index.html into two separate files:
    page_layout contains all the background imagery and aspects common to the overall design
    
    search extends the page_layout page and implements a series of for loops populating search boxes from data passed via the endpoint in WiFinderApp.py
    
    WiFinderApp.py has two new endpoints to point to these new pages. Layout is purely for illustrative purposes and is never intended to be accessed by end users.
    
    The search endpoints data inputs are all illustrative and are intended to be replaced by data returned from requests against the database.

--- app/website/WiFinderApp.py
+++ app/website/WiFinderApp.py
@@ -16,5 +16,23 @@ def connectDB():
 	'''DB connection'''
 	return sqlite3.connect(WiFinderApp.db)
 
+@WiFinderApp.route("/search")
+def search_page():
+	'''load search page'''
+
+	return render_template("search.html",
+						   title = 'Search',
+						   rooms = ['B002', 'quack', 'B004'],
+						   times = [9, 10, 11, 12 ,13 ,14 ,15 ,16 ,17, 18, 19],
+						   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+						   densities = ['0%', '25%', '50%', '75%', '100%'])
+
+@WiFinderApp.route("/layout")
+def layout():
+	'''load base layout'''
+
+	return render_template("page_layout.html",
+						   title = 'Layout')
+
 if __name__ == "__main__":
 	WiFinderApp.run()
