9090068776534e7b61942164fdb20ec581f24b06
Mike <mikemorrisroe@gmail.com>
2016-07-05 15:35:12 +0100

    changed timetable db prep to create new csvs in the planned schema format. iterates through list of timetables now and generates an overall timetable file, a room file and a module file.

--- data_process_scripts/timetable_db_prep.py
+++ data_process_scripts/timetable_db_prep.py
@@ -1,72 +1,99 @@
 
 # coding: utf-8
 
-
-
 import pandas as pd
+import os
+
+os.chdir('/home/mike/wifiproj/data')
+
+timetables = ['B0.02_ready', 'B0.03_ready', 'B0.04_ready']
+cleantables = []
 
 
-df = pd.read_csv('B002_finalmerge.csv')
+for timetable in timetables:
 
-df2 = df.ix[:9, :11]
+    df = pd.read_csv('{}.csv'.format(timetable))
 
-del df
-
-
-
-mon_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
-
-tue_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
-
-wed_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
-
-thu_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
-
-fri_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
-
-day_tables = [mon_df, tue_df, wed_df, thu_df, fri_df]
+    df = df.ix[:9, :11]
 
 
-n = 1
 
-for table in day_tables:
-    table.Time = df2[[0]].ix[1:]
-    table.Module = df2[[n]].ix[1:]
-    table.Registered_Students = df2[[n+1]].ix[1:]
-    room = df2.columns[0]
-    csroom = 'CS' + room
-    table.Room= csroom
-    table.Room_Capacity = df2.columns[2].split(' ')[2]
-    # date. changeable
-    n += 2
+    mon_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
 
-del n
+    tue_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
+
+    wed_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
+
+    thu_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
+
+    fri_df = pd.DataFrame(index = range(10), columns=['Time', 'Date', 'Room', 'Room_Capacity', 'Module', 'Registered_Students'])
+
+    day_tables = [mon_df, tue_df, wed_df, thu_df, fri_df]
 
 
-B002_timetable = pd.concat(day_tables)
+    n = 1
 
-B002_timetable = B002_timetable[B002_timetable.Time.notnull()]
+    for table in day_tables:
+        table.Time = df[[0]].ix[1:]
+        table.Module = df[[n]].ix[1:]
+        table.Registered_Students = df[[n+1]].ix[1:]
+        room = df.columns[0]
+        csroom = 'CS' + room
+        table.Room= csroom
+        table.Room_Capacity = df.columns[2].split(' ')[2]
+        # date. changeable
+        n += 2
 
-two_weeks = [B002_timetable, B002_timetable]
-
-B002_timetable = pd.concat(two_weeks)
-
-date_index = pd.date_range('2015-11-2', periods=12, freq='D')
-
-
-day = 0
-count = 0
-
-while count < B002_timetable.shape[0]:
-    B002_timetable.iat[count,1] = date_index[day]
-
-    if (count + 1)% 9 == 0:
-        day += 1
-
-        if day == 5:
-            day += 2
-
-    count += 1
+    del n
 
 
-B002_timetable.reset_index(drop=True)
+    df = pd.concat(day_tables)
+
+    df = df[df.Time.notnull()]
+
+    two_weeks = [df, df]
+
+    df = pd.concat(two_weeks)
+
+    date_index = pd.date_range('2015-11-2', periods=12, freq='D')
+
+
+    day = 0
+    count = 0
+
+    while count < df.shape[0]:
+        df.iat[count,1] = date_index[day]
+
+        if (count + 1)% 9 == 0:
+            day += 1
+
+            if day == 5:
+                day += 2
+
+        count += 1
+
+    count = 0
+
+    while count < df.shape[0]:
+        if pd.isnull(df.iat[count,4]):
+            df.iat[count,4] = 'Vacant'
+            df.iat[count,5] = 'Vacant'
+
+        count += 1
+
+    print df
+
+    cleantables.append(df)
+
+df.reset_index(drop=True)
+
+df_finish = pd.concat(cleantables)
+print df_finish
+
+df_finish.to_csv('timetable_table.csv')
+
+roomtable = df[['Room', 'Room_Capacity']]
+modtable = df[['Module', 'Registered_Students']]
+
+roomtable.to_csv('room_table.csv')
+modtable.to_csv('mod_table.csv')
