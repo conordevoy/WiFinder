ebe369b744c679453c489f2b3241d74a90613c3d
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-07-18 12:25:24 +0100

    Updated DB with occupancy data

--- Data/final_csvs/db_maker.py
+++ Data/final_csvs/db_maker.py
@@ -2,7 +2,7 @@ import os
 import csv
 import sqlite3
 
-conn = sqlite3.connect('WiFinderDBv01.db')
+conn = sqlite3.connect('WiFinderDBv02.db')
 cur = conn.cursor()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS ROOM
@@ -31,6 +31,16 @@ cur.execute('''CREATE TABLE IF NOT EXISTS WIFI_LOGS
 	FOREIGN KEY(Room) REFERENCES ROOM(Room),
 	FOREIGN KEY(ClassID) REFERENCES CLASS(ClassID));''')
 
+cur.execute('''CREATE TABLE IF NOT EXISTS OCCUPANCY
+	(OccID INTEGER PRIMARY KEY AUTOINCREMENT,
+	Hour INT NOT NULL,
+	Datetime TEXT NOT NULL,
+	Room TEXT NOT NULL,
+	Occupancy REAL NOT NULL,
+	ClassID TEXT NOT NULL,
+	FOREIGN KEY(Room) REFERENCES ROOM(Room),
+	FOREIGN KEY(ClassID) REFERENCES CLASS(ClassID));''')
+
 with open('room_table.csv', 'rt') as f:
 	dr = csv.DictReader(f)
 	to_db_room = [(i['Room'], i['Capacity'], i['Building'], i['Campus']) for i in dr]
@@ -46,8 +56,14 @@ with open ('logdata_table.csv', 'rt') as f:
 	to_db_log = [(i['Time'], i['Hour'], i['Date'], i['Room'], i['Associated_count'], i['ID']) for i in dr]
 f.close()
 
+with open('occupancy_table_final.csv', 'rt') as f:
+	dr = csv.DictReader(f)
+	to_db_occ = [(i['Hour'], i['Date'], i['Room'], i['Occupancy'], i['ID']) for i in dr]
+f.close()
+
 cur.executemany('INSERT INTO ROOM (Room, Capacity, Building, Campus) VALUES (?, ?, ?, ?);', to_db_room)
 cur.executemany('INSERT INTO CLASS (ClassID, Hour, Datetime, Room, Module, Reg_Students) VALUES (?, ?, ?, ?, ?, ?);', to_db_class)
-cur.executemany('INSERT INTO WIFI_LOGS (Time, Hour, Datetime, Room, Log_Count, ClassID) VALUEs (?, ?, ?, ?, ?, ?);', to_db_log)
+cur.executemany('INSERT INTO WIFI_LOGS (Time, Hour, Datetime, Room, Log_Count, ClassID) VALUES (?, ?, ?, ?, ?, ?);', to_db_log)
+cur.executemany('INSERT INTO OCCUPANCY (Hour, Datetime, Room, Occupancy, ClassID) VALUES (?, ?, ?, ?, ?)', to_db_occ)
 conn.commit()
 conn.close()
