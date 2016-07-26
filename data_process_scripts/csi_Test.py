883e60a0aa6fea7657a9f0aba6d8b5ca9a66841e
Mike <mikemorrisroe@gmail.com>
2016-07-26 15:41:00 +0100

    no idea

--- data_process_scripts/csi_Test.py
+++ data_process_scripts/csi_Test.py
@@ -1,58 +1,59 @@
 import openpyxl
 import csv
-import os
 import re
 
-path_choice = '/home/mike/wifiproj/data' # local path, just to speed up script
-os.chdir(path_choice)
 
-wb = openpyxl.load_workbook('CSI Occupancy report.xlsx')
-sheet = wb.get_sheet_by_name('CSI')
+def clean_CSI_csv_files():
 
-with open('CSI.csv', 'w') as f:
-        c = csv.writer(f)
-        for r in sheet.rows:
-            c.writerow([cell.value for cell in r])
+    wb = openpyxl.load_workbook('CSI Occupancy report.xlsx') # load correct workbook
+    sheet = wb.get_sheet_by_name('CSI') # pull CSI sheet as it is the only important one
 
-csi = open('CSI.csv')
-csi_reader = csv.reader(csi)
+    # create CSI.csv file, and iterate through each row, writing the cell value into the new csv
+    with open('CSI.csv', 'w') as f:
+            c = csv.writer(f)
+            for r in sheet.rows:
+                c.writerow([cell.value for cell in r])
+
+    csi = open('CSI.csv') # open new .csv file
+    csi_reader = csv.reader(csi) # create reader object
 
 
-csi_string = ''
-for row in csi_reader:
-    csi_string += ', '.join(row)
-    csi_string += '\n'
+    csi_string = '' # create empty string for storing elements of csv
+    for row in csi_reader:
+        csi_string += ', '.join(row) # join each row's elements with a comma and a space
+        csi_string += '\n' # join each line with a newline to maintain original structure
 
-    if 'Poor' in row: # stop 15000 lines of nothing
-        break
+        if 'Poor' in row: # stop 15000 lines of nothing
+            break
 
-mylist = []
-match_list = []
+    csi_row_list = []
+    match_list = []
 
-mylist = csi_string.split('\n')
+    csi_row_list = csi_string.split('\n')
 
-start_match = re.compile(r'CSI Classroom OCCUPANCY')
-fin_match = re.compile(r'CSI Classroom UTILISATION')
-blank_match = re.compile(r'^(, , , ,)')
+    # create regex match patterns
+    start_match = re.compile(r'CSI Classroom OCCUPANCY') # for matching start of relevant block
+    fin_match = re.compile(r'CSI Classroom UTILISATION') # for matching end of relevant block
+    blank_match = re.compile(r'^(, , , ,)') # for matching blank lines
 
-start_flag = None
-blank_flag = None
+    start_flag = None # tracks when in relevant block
+    blank_flag = None # tracks if blank row detected
 
-for row in mylist:
-    if start_match.match(row): start_flag = True ; continue
-    if fin_match.match(row): start_flag = False
-    if blank_match.match(row): blank_flag = True
+    for row in csi_row_list:
+        if start_match.match(row): start_flag = True ; continue
+        if fin_match.match(row): start_flag = False
+        if blank_match.match(row): blank_flag = True
 
-    if blank_flag:
-        blank_flag = False
-        continue
+        if blank_flag:
+            blank_flag = False
+            continue
 
-    if start_flag:
-        match_list.append(row)
+        if start_flag:
+            match_list.append(row)
 
-# for i in match_list:
-#     print(i)
+    # for i in match_list:
+    #     print(i)
 
-with open('CSI_clean.csv', 'w') as f:
-        for item in match_list:
-            f.writelines(item + '\n')
+    with open('CSI_clean.csv', 'w') as f:
+            for item in match_list:
+                f.writelines(item + '\n')
