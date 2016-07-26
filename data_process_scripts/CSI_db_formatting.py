7a5bc08241f4b6893a935c7062e7d6835d47e74d
Mike <mikemorrisroe@gmail.com>
2016-07-26 15:35:57 +0100

    exported some functions and added some formatting from merged notebooks

--- data_process_scripts/CSI_db_formatting.py
+++ data_process_scripts/CSI_db_formatting.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import re
+import data_process_scripts.functions as fn
+
 
 
 def prep_CSI_db_files():
@@ -11,7 +13,6 @@ def prep_CSI_db_files():
     df2 = df2.drop(df2.columns[1], axis=1)
     df2 = df2.drop(df2.columns[2], axis=1)
 
-    table_struc = pd.DataFrame(index=range(120), columns=['Time', 'Date', 'Room', 'Occupancy'])
     b4_table = pd.DataFrame(index=range(120), columns=['Time', 'Date', 'Room', 'Occupancy'])
     b2_table = pd.DataFrame(index=range(120), columns=['Time', 'Date', 'Room', 'Occupancy'])
     b3_table = pd.DataFrame(index=range(120), columns=['Time', 'Date', 'Room', 'Occupancy'])
@@ -58,5 +59,20 @@ def prep_CSI_db_files():
     room_df = pd.concat(room_tables)
 
     room_df = room_df[room_df.Time.notnull()]
+
+    # convert Date to datetime format
+    room_df['Date'] = pd.to_datetime(room_df['Date'])
+
+    #keep only first 2 characters in time column and remove .
+    room_df['Time'] = room_df['Time'].map(lambda x: str(x)[:2])
+    room_df['Time'] = room_df['Time'].map(lambda x: x.lstrip('.').rstrip('.'))
+
+    #only keep date in Date column
+    room_df['Date'] = room_df['Date'].apply(lambda x: x.date())
+
+    #rename columns
+    room_df.columns = ['Hour','Date','Room','Occupancy']
+
+    fn.set_identifier(room_df)
 
     room_df.to_csv('occupancy_table.csv', index=False)
