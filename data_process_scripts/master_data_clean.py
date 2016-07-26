d3d4091d6ed8774f2442d3c13e3aaa3752882db7
Mike <mikemorrisroe@gmail.com>
2016-07-26 15:36:37 +0100

    reorganised functions

--- data_process_scripts/master_data_clean.py
+++ data_process_scripts/master_data_clean.py
@@ -4,31 +4,11 @@ from data_process_scripts.timetable_db_prep import timetables_to_csv
 from data_process_scripts.log_file_cleaner import clean_log_files
 from data_process_scripts.csi_Test import clean_CSI_csv_files
 from data_process_scripts.CSI_db_formatting import prep_CSI_db_files
+import data_process_scripts.functions as fn
 
 
 
-
-def check_path():
-
-    print('The current working directory is:', os.getcwd())
-    path = input('If this is correct, press enter now. \n'
-                 'Otherwise, press any key followed by enter.\n')
-
-
-    if path != '':
-        path_choice = input('Please enter the directory to use: \n')
-        invalid_path = True
-
-        while invalid_path:
-            try:
-                os.chdir(path_choice)
-                invalid_path = False
-            except:
-                path_choice = input('Invalid path. Please enter the directory to use: \n')
-
-    return path_choice
-
-check_path()
+fn.check_path()
 path_choice = os.getcwd()
 
 # clean the timetables
