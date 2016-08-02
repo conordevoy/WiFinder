e739fc1cb63866cbdf091ea9a5bb99725d467780
Mike <mikemorrisroe@gmail.com>
2016-08-02 21:28:54 +0100

    added estimator endpoint needs to be rewritten but mostly works for now

--- app/website/WiFinderApp.py
+++ app/website/WiFinderApp.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, g, redirect, url_for, request, session, flash
 from functools import wraps
 import sqlite3
-from Modelling_Functions import *
+from app.website.Modelling_Functions import logistic_classifier
+from app.website.Modelling_Functions import test
 
 WiFinderApp = Flask(__name__, static_url_path="/static")
 
@@ -10,6 +11,12 @@ WiFinderApp.debug = True
 WiFinderApp.secret_key = '\xbf\xb0\x11\xb1\xcd\xf9\xba\x8b\x0c\x9f' #session key random generated from os
 
 db = "WiFinderDBv02.db"
+
+stored_query = """SELECT AVG(W.Log_Count)
+        FROM WIFI_LOGS W JOIN CLASS C ON W.ClassID = C.ClassID
+        JOIN ROOM R ON C.Room = R.RoomID JOIN OCCUPANCY O ON R.RoomID = O.Room
+        WHERE R.RoomID = \'{}\' AND W.Datetime = \'{}\' AND W.Hour = \'{}\'
+        AND strftime('%M', W.Time) BETWEEN \"15\" AND \"45\";"""
 
 #login required decorator
 def login_required(f):
@@ -95,6 +102,66 @@ def results():
     return render_template("results.html", 
                             title='Results',
                             result=subdata.fetchone()[0])
+
+@WiFinderApp.route("/estimator", methods=['GET'])
+def estimator():
+    '''estimates three model functions'''
+
+    timedata = query("SELECT DISTINCT Hour FROM CLASS;")
+    roomdata = query("SELECT DISTINCT RoomID FROM ROOM;")
+    moduledata = query("SELECT DISTINCT Module FROM CLASS;")
+    datedata = query("SELECT DISTINCT Datetime FROM WIFI_LOGS;")
+
+    # get values from form
+    room = request.args.get('Room')
+    datetime = request.args.get('Date')
+    time = request.args.get('Time')
+
+
+    cur = get_db().cursor()
+    average_counts = cur.execute("""SELECT AVG(W.Log_Count)
+        FROM WIFI_LOGS W JOIN CLASS C ON W.ClassID = C.ClassID
+        JOIN ROOM R ON C.Room = R.RoomID JOIN OCCUPANCY O ON R.RoomID = O.Room
+        WHERE R.RoomID = \'{}\' AND W.Datetime = \'{}\' AND W.Hour = \'{}\'
+        AND strftime('%M', W.Time) BETWEEN \"15\" AND \"45\";""".format(room, datetime, time))
+
+    query_result = average_counts.fetchone()[0]
+
+    glyph_dict = {'Empty' : 'remove-circle', # these aren't strictly necessary at the moment
+    				'Occupied': 'ok-circle', # however, i'm going to retool the results page to be shorter
+    				'Low' : 'arrow-down', # and then these will be less useless
+    				'Medium': 'arrow-right',
+    				'High': 'arrow-up'}
+
+
+    if room and time and datetime:
+        linear_estimate = test(query_result)
+        tertiary_estimate = logistic_classifier(query_result, 'tertiary')
+        binary_estimate = logistic_classifier(query_result, 'binary')
+
+    # these return statements are ugly - these will hopefully be fixed but work for now.
+        # maybe url redirects would be better?
+        return render_template("estimator.html",
+                           title='Estimations',
+                           linear_estimate = linear_estimate,
+                           binary_estimate = binary_estimate,
+                           tertiary_estimate = tertiary_estimate,
+                           binary_glyph = glyph_dict[binary_estimate],
+                           tertiary_glyph = glyph_dict[tertiary_estimate],
+                           rooms=roomdata,
+                           times=timedata,
+                           modules=moduledata,
+                           dates=datedata
+                           )
+
+    else:
+        return render_template("estimator.html",
+                   title='Estimations',
+                   rooms=roomdata,
+                   times=timedata,
+                   modules=moduledata,
+                   dates=datedata
+                   )
 
 @WiFinderApp.route("/layout")
 def layout():
