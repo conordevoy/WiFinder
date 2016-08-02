0feaf0aa8183052e29f1cb94658e13680dcaf5de
Mike <mikemorrisroe@gmail.com>
2016-08-02 21:31:49 +0100

    rewrote function and added replica function to make the linear predictor work.

--- app/website/Linear_Regression_Creator.py
+++ app/website/Linear_Regression_Creator.py
@@ -27,116 +27,11 @@ for row in range(df.shape[0]):
     headcount_estimate = (df.iat[row, survey] * df.iat[row, room_cap])
     df.iat[row, headcount_col] = headcount_estimate
 
-
-                                 #         Linear Regressions 1
-                                 #
-                                 #          Predict: Occupancy
-                                 #
-                                 #
-                                 #              Features:
-                                 #
-                                 #      Average Count over 60 minutes
-                                 #      Average Count over 30 minutes
-                                 # Adjusted Average Count over 60 minutes
-                                 # Adjusted Average Count over 30 minutes
-
-
-lm = sm.ols(formula="Occupancy ~ Avg_Count_60min", data=df).fit()
-
-print(lm.summary())
-
-lm = sm.ols(formula="Occupancy ~ Avg_Count_30min", data=df).fit()
-
-print(lm.summary())
-
-lm = sm.ols(formula="Occupancy ~ Avg_Adj_Count_60min", data=df).fit()
-
-print(lm.summary())
-
-lm = sm.ols(formula="Occupancy ~ Avg_Adj_Count_30min", data=df).fit()
-
-print(lm.summary())
-
-                            #
-                            #              Linear Regressions 2
-                            #
-                            # Adjusted30min is the highest scoring on Occupancy.
-                            #
-                            #               Predict: Occupancy
-                            #
-                            #                   Features:
-                            #
-                            #           Average Count over 60 minutes
-                            #           Average Count over 30 minutes
-                            #      Adjusted Average Count over 60 minutes
-                            #      Adjusted Average Count over 30 minutes
-
-
-lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Avg_Count_60min", data=df).fit()
-
-print(lm.summary())
-
-lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Avg_Count_30min", data=df).fit()
-
-print(lm.summary())
-
-lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Avg_Adj_Count_60min", data=df).fit()
-
-print(lm.summary())
-
-lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Avg_Adj_Count_30min", data=df).fit()
-
-print(lm.summary())
-
-
-                       #                       Linear Results 3:
-                       #
-                       #  Adjusted30min is the highest scoring on occupancy.
-                       #  Avg30min is the highest scoring on headcount_estimate.
-                       #
-                       # Next, we attempt normalisation to deal with outliers.
-                       # We repeat the previous two winning regressions from each step.
-
-
-df_norm = df
-
-df_norm['Norm_Avg_Adj_Count_30min'] = df_norm['Avg_Adj_Count_30min']
-df_norm['Norm_Avg_Count_30min'] = df_norm['Avg_Count_30min']
-
-cols_to_norm = ['Norm_Avg_Count_30min', 'Norm_Avg_Adj_Count_30min']
-df[cols_to_norm] = df[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
-
-
-
-# OLS with normalised adjusted 30minute count, predicting occupancy
-
-lm = sm.ols(formula="Occupancy ~ Norm_Avg_Adj_Count_30min", data=df).fit()
-
-print(lm.summary())
-
-# OLS with normalised 30minute count, predicting headcount
-
-lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Norm_Avg_Count_30min", data=df).fit()
-
-print(lm.summary())
-
-test_residuals = lm.resid
-
-# plot residuals
-
-lm.resid.plot()
-
-# residuals are significantly peaked, especially at the higher end of the occupancy scale
-
-
-# test fitting residual series to model and then removing >3 sd instances and replotting residuals.
-
-
 test_residuals = df
 
 # plot best model - predict headcount from 30min average associated count
 
-lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Norm_Avg_Count_30min", data=test_residuals).fit()
+lm = sm.ols(formula="Room_Survey_Headcount_Estimate ~ Avg_Count_30min", data=test_residuals).fit()
 
 print(lm.summary())
 
