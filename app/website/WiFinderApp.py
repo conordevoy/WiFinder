3152f6e1ee7351c893bc025d379657fa68054955
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-08-02 15:10:53 +0100

    fixed sql query for 15/15 average logs, removed average function

--- app/website/WiFinderApp.py
+++ app/website/WiFinderApp.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, g, redirect, url_for, request, session, flash
 from functools import wraps
 import sqlite3
-# import flask
-# import sqlite3 as lite
-# from bokeh.charts import Line
-# import pandas as pd
-# from bokeh.embed import components
-# from bokeh.resources import INLINE
 from average_log import average_log_count
 
 WiFinderApp = Flask(__name__, static_url_path="/static")
@@ -93,14 +87,14 @@ def results():
     time = request.args.get('Time')
     print(room, datetime, time)
     cur = get_db().cursor()
-    alldata = cur.execute("""SELECT W.LogID, W.Time, W.Hour, W.Datetime, W.Log_Count, R.RoomID, R.Capacity, C.ClassID, C.Module, C.Reg_Students, O.Occupancy 
-                    FROM WIFI_LOGS W JOIN CLASS C ON W.ClassID = C.ClassID JOIN ROOM R ON C.Room = R.RoomID JOIN OCCUPANCY O ON R.RoomID = O.Room 
-                    WHERE R.RoomID = \'%s\' AND W.Datetime = \'%s\' AND W.Hour = \'%s\' 
-                    GROUP BY W.LogID;""" % (room, datetime, time))
-    result = average_log_count(alldata)
+    subdata = cur.execute("""SELECT AVG(W.Log_Count) 
+        FROM WIFI_LOGS W JOIN CLASS C ON W.ClassID = C.ClassID 
+        JOIN ROOM R ON C.Room = R.RoomID JOIN OCCUPANCY O ON R.RoomID = O.Room 
+        WHERE R.RoomID = \'{}\' AND W.Datetime = \'{}\' AND W.Hour = \'{}\' 
+        AND strftime('%M', W.Time) BETWEEN \"15\" AND \"45\";""".format(room, datetime, time))
     return render_template("results.html", 
                             title='Results',
-                            result=result)
+                            result=subdata.fetchone()[0])
 
 @WiFinderApp.route("/layout")
 def layout():
