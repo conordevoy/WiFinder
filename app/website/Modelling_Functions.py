8078e5be723decb1926fe545daf8d4e69786cb6e
Mike <mikemorrisroe@gmail.com>
2016-08-02 21:28:32 +0100

    rewrote a function literally the exact same way so that it would work

--- app/website/Modelling_Functions.py
+++ app/website/Modelling_Functions.py
@@ -25,45 +25,44 @@ def linear_prediction(predictor_value):
 
     return result
 
-def estimate_occupancy_number(query_result):
+def test(count):
 
-    predictor_value = prep_predictor_value(query_result)
+    count_in_dataframe = prep_predictor_value(count)
+    predicted_value = linear_prediction(count_in_dataframe)
 
-    occupancy_estimate = linear_prediction(predictor_value)
+    return predicted_value
 
-    return occupancy_estimate
-
-def prep_predictor_values(*query_results):
-    """Wraps MULTIPLE values in a pd.DataFrame to prepare it for a StatsModels Linear Regression object
-
-    Avg_Count_30min is the name the model was trained to predict on."""
-
-    query_list = []
-    for result in query_results:
-        query_list.append(result)
-
-    predictor_value = DataFrame({'Avg_Count_30min': query_list})
-
-    return predictor_value
-
-def linear_predictions(predictor_values):
-    """Performs a prediction on a supplied dataframe object and returns the result"""
-
-    result = linear_estimate_headcount.predict(predictor_values)
-
-    list(result)
-    result = [int(x) for x in result]
-
-    return result
-
-def estimate_occupancy_numbers(*query_results):
-    """THROWS ERROR - no dtypes ; no idea why"""
-
-    predictor_values = prep_predictor_values(query_results)
-
-    occupancy_estimates = linear_predictions(predictor_values)
-
-    return occupancy_estimates
+# def prep_predictor_values(*query_results):
+#     """Wraps MULTIPLE values in a pd.DataFrame to prepare it for a StatsModels Linear Regression object
+#
+#     Avg_Count_30min is the name the model was trained to predict on."""
+#
+#     query_list = []
+#     for result in query_results:
+#         query_list.append(result)
+#
+#     predictor_value = DataFrame({'Avg_Count_30min': query_list})
+#
+#     return predictor_value
+#
+# def linear_predictions(predictor_values):
+#     """Performs a prediction on a supplied dataframe object and returns the result"""
+#
+#     result = linear_estimate_headcount.predict(predictor_values)
+#
+#     list(result)
+#     result = [int(x) for x in result]
+#
+#     return result
+#
+# def estimate_occupancy_numbers(*query_results):
+#     """THROWS ERROR - no dtypes ; no idea why"""
+#
+#     predictor_values = prep_predictor_values(query_results)
+#
+#     occupancy_estimates = linear_predictions(predictor_values)
+#
+#     return occupancy_estimates
 
 
 # logistic functions
