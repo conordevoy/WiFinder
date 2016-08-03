18a98cd15604f5a9c21daa565b29c58624cf72b1
Mike <mikemorrisroe@gmail.com>
2016-08-03 21:48:48 +0100

    I don't think I changed anything here but git says so

--- data_process_scripts/master_data_clean.py
+++ data_process_scripts/master_data_clean.py
@@ -2,7 +2,7 @@ import os
 from data_process_scripts.straight_line_timetable import clean_tables
 from data_process_scripts.timetable_db_prep import timetables_to_csv
 from data_process_scripts.log_file_cleaner import clean_log_files
-from data_process_scripts.csi_Test import clean_CSI_csv_files
+from data_process_scripts.csi_clean_csvs import clean_CSI_csv_files
 from data_process_scripts.CSI_db_formatting import prep_CSI_db_files
 import data_process_scripts.functions as fn
 from data_process_scripts.log_cleaner_db_prep import clean_log_files
