ba2211473e6905cd98d7fbdcc2ea19169fa1193d
Mike <mikemorrisroe@gmail.com>
2016-08-09 18:03:07 +0100

    added /explore2 endpoint this connects to the new explore page and handles the three selector variables. uses a lot of silly logic functions.
    currently very messy but is returning correct sql statements to retrieve the relevant information.

--- app/website/WiFinderApp.py
+++ app/website/WiFinderApp.py
@@ -176,8 +176,10 @@ def estimator():
                                rooms=roomdata,
                                times=timedata,
                                modules=moduledata,
-                               dates=datedata
-                               )
+                               dates=datedata,
+                               room_check=room,
+                               date_check=datetime,
+                               time_check=time)
 
     else:
         return render_template("estimator.html",
@@ -185,8 +187,7 @@ def estimator():
                                rooms=roomdata,
                                times=timedata,
                                modules=moduledata,
-                               dates=datedata
-                               )
+                               dates=datedata)
 
 
 @WiFinderApp.route("/updatemodel", methods=['GET'])
@@ -235,12 +236,159 @@ def data_input():
     return render_template("data_input.html",
             current_files= cf, title='Data Input')
 
-@WiFinderApp.route("/explore")
-def explore():
-    timedata = query("SELECT DISTINCT Hour FROM CLASS;")
-    roomdata = query("SELECT DISTINCT RoomID FROM ROOM;")
-    moduledata = query("SELECT DISTINCT Module FROM CLASS;")
-    datedata = query("SELECT DISTINCT Datetime FROM CLASS;")
+@WiFinderApp.route("/explore2")
+def explore2():
+
+    def true_count(a, b, c): return int(bool(a)) + int(bool(b)) + int(bool(c))
+
+    def select_time_value(day, date, hour):
+        if true_count(day, date, hour) != 1:
+            time_error = "Please choose exactly one time value."
+            print(time_error)
+
+
+        if true_count(day, date, hour) == 1:
+                if bool(day) == 1:
+                    return "AND strftime('%w', w.Datetime) = {day}".format(day=day)
+                if bool(date) == 1:
+                    return 'AND w.Datetime = "{date}"'.format(date=date)
+                if bool(hour) == 1:
+                    return "AND  strftime('%H', w.Time) = {hour}".format(hour=hour)
+
+    def select_filter_value(room, module):
+        if true_count(room, module, None) > 1:
+            filter_error = "Please choose exactly one filter value."
+            print(filter_error)
+            return 'ERROR IN FILTER FUNCTION' + str(true_count(room, module, None)) + str(room) + str(module)
+
+        if true_count(room, module, None) == 1:
+                if bool(room) == 1:
+                    return "AND r.RoomID = '{room}'".format(room=room)
+                if bool(module) == 1:
+                    return "AND c.Module = {module}".format(module=module)
+
+    def select_y_axis_value(occupancy_requested, count_requested, capacity_requested):
+
+        stem = ''
+        if bool(occupancy_requested) == 1:
+            stem += 'o.Occupancy' + ', '
+        if bool(count_requested) == 1:
+            stem += 'w.Log_Count' + ', '
+        if bool(capacity_requested) == 1:
+            stem += 'r.Capacity'
+
+        if stem.count(",") >= stem.count("."):
+            index = stem.rfind(",")
+            stem = stem[:index] + stem[index + 1:]
+
+        return stem
+
+
+    day_dict = {'Monday': 1,
+                'Tuesday': 2,
+                'Wednesday': 3,
+                'Thursday': 4,
+                'Friday': 5}
+
+    rooms_available = query("SELECT DISTINCT RoomID FROM ROOM;")
+    modules_available = query("SELECT DISTINCT Module FROM CLASS;")
+    dates_available = query("SELECT DISTINCT Datetime FROM CLASS;")
+    hours_available = query("SELECT DISTINCT Hour FROM CLASS")
+
+
+    room_value = request.args.get('Room')
+    date_value = request.args.get('Date')
+    day_value = request.args.get('Day')
+    hour_value = request.args.get('Hour')
+    module_value = request.args.get('Module')
+    occupancy_requested = request.args.get('Occupancy_check')
+    capacity_requested = request.args.get('Capacity_check')
+    count_requested = request.args.get('Count_check')
+
+    time_value = select_time_value(day_value, date_value, hour_value)
+    filter_value = select_filter_value(room_value, module_value)
+    yaxis_value = select_y_axis_value(occupancy_requested, count_requested, capacity_requested)
+
+    if true_count(time_value, filter_value, yaxis_value) == 3:
+        explore_query = """SELECT {y_axis_selector}
+                            FROM WIFI_LOGS w JOIN OCCUPANCY o JOIN ROOM r JOIN CLASS c
+                            WHERE o.ClassID = w.ClassID
+                            AND c.ClassID = o.ClassID
+                            AND r.RoomID = o.Room
+                            {time_selector}
+                            {filter_selector}
+                            """.format(y_axis_selector=yaxis_value,
+                                       time_selector=time_value,
+                                       filter_selector=filter_value)
+
+        return render_template("explore2.html",
+                                rooms=rooms_available,
+                                modules=modules_available,
+                                dates=dates_available,
+                                days=day_dict,
+                                hours=hours_available,
+                               returned_data=explore_query,
+                                which_path=true_count(time_value, filter_value, yaxis_value),
+                               values = ['room', room_value,
+                                     'module',
+                                     module_value,
+                                     'date', date_value,
+                                     'day', day_value,
+                                     'hour', hour_value,
+                                    'occupancy', occupancy_requested,
+                                     'capacity', capacity_requested,
+                                     'count', count_requested,
+                                    'time', time_value,
+                                     'filter', filter_value,
+                                     'yaxis', yaxis_value])
+
+    if 1 <= true_count(time_value, filter_value, yaxis_value) < 3 :
+        selection_error = 'Please select one value from each list.'
+
+        return render_template("explore2.html",
+                           rooms=rooms_available,
+                           modules=modules_available,
+                           dates=dates_available,
+                           days=day_dict,
+                           hours=hours_available,
+                            error = selection_error,
+                               which_path=true_count(time_value, filter_value, yaxis_value),
+                               values = ['room', room_value,
+                                     'module',
+                                     module_value,
+                                     'date', date_value,
+                                     'day', day_value,
+                                     'hour', hour_value,
+                                    'occupancy', occupancy_requested,
+                                     'capacity', capacity_requested,
+                                     'count', count_requested,
+                                    'time', time_value,
+                                     'filter', filter_value,
+                                     'yaxis', yaxis_value])
+
+
+    return render_template("explore2.html",
+                           rooms=rooms_available,
+                           modules=modules_available,
+                           dates=dates_available,
+                           days=day_dict,
+                           hours=hours_available,
+                           which_path = true_count(time_value, filter_value, yaxis_value),
+                           values = ['room', room_value,
+                                     'module',
+                                     module_value,
+                                     'date', date_value,
+                                     'day', day_value,
+                                     'hour', hour_value,
+                                    'occupancy', occupancy_requested,
+                                     'capacity', capacity_requested,
+                                     'count', count_requested,
+                                    'time', time_value,
+                                     'filter', filter_value,
+                                     'yaxis', yaxis_value])
+
+
+
 
 @WiFinderApp.route("/layout")
 def layout():
